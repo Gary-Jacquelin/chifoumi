8fcbc4e20d626ebc30b2e6d4f67153a9167bb6e6
unknown <Gary@EGL-2021-A3.energygo.local>
2023-02-14 09:59:20 +0100

    vite_moussa

--- vite.config.js
+++ vite.config.js
@@ -8,38 +8,39 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      manifest: {
-        theme_color: "#f69435",
-        background_color: "#f69435",
-        display: "browser",
-        scope: "/",
-        start_url: "/",
-        name: "schifumi",
-        short_name: "schifumi",
-        description: "",
-        icons: [
-          {
-            src: "/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
+       manifest: {
+      "theme_color": "#f6cc35",
+      "background_color": "#f69435",
+      "display": "fullscreen",
+      "scope": "/",
+      "start_url": "/",
+      "name": "Simon Says",
+      "short_name": "Simon Says",
+      "description": "play simon say",
+      "orientation": 'landscape',
+      "icons": [
+        {
+          "src": "/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    },
       devOptions: {
         enabled: true
       }
